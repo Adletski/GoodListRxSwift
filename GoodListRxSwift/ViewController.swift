4af5a7fd302d967f43e76c6129d64f8258ef069f
Adlet <adlet.zh19966@gmail.com>
2024-11-13 17:52:12 +0600

    Initial Commit

---
+++ GoodListRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodListRxSwift
+//
+//  Created by Adlet Zhantassov on 13.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
